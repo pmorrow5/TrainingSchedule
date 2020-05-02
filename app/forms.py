1e23e6e1dc6121b4da9e867b20ecdc74cc17f3c3
Patrick Morrow <patrick@powderhook.com>
2020-05-02 17:31:58 -0500

    fix validation on sign in

--- app/forms.py
+++ app/forms.py
@@ -9,16 +9,6 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
