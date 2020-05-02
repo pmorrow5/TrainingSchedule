583c04d23b3abd7b8ea388ccf39d394f2b23fa40
Patrick Morrow <patrick@powderhook.com>
2020-05-02 15:34:32 -0500

    Add database and user login

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# Load config
 app.config.from_object(Config)
+
+# Load database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Load login manager
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
